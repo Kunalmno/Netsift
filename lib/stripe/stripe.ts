6fcc3385ef9d625a7aaaf7fc9976aed41f894701
Kunalmno <Kunalpandeykanu@gmail.com>
2025-04-30 19:36:07 +0530

    .

---
+++ lib/stripe/stripe.ts
@@ -0,0 +1,7 @@
+import { Key } from "lucide-react";
+import Stripe from "stripe";
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2025-03-31.basil",
+  typescript: true,
+});
